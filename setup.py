bc7537663fc4ce2ddee356dd6f98c5eaeca996a8
antoninsiska <siska.antonin.mail@gamil.com>
2025-08-26 21:35:01 +0200

    rename

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="fpsdemo",
+    name="game-engine",
     version="0.2.0",
     author="Antonín Šiška",
     author_email="siska.antonin.mail@example.com",
     description="3D FPS demo s PyQt6, kolizemi a minimapou",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/tvuj-github-username/fpsdemo",
+    url="https://github.com/tvuj-github-username/game-engine",
     packages=find_packages(),
     install_requires=["PyQt6>=6.5.0"],
     entry_points={
         "console_scripts": [
-            "fpsdemo=fpsdemo.main:main",
+            "game-engine=game-engine.main:main",
         ],
     },
     classifiers=[
