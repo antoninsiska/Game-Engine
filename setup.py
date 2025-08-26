a7892dbd7e9b202a8f06a49a7c698c53b853336c
antoninsiska <siska.antonin.mail@gamil.com>
2025-08-26 21:39:23 +0200

    fix issue with names

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
 from setuptools import setup, find_packages
 
+try:
+    with open("README.md", encoding="utf-8") as f:
+        long_desc = f.read()
+except FileNotFoundError:
+    long_desc = ""
+
 setup(
     name="game-engine",
     version="0.2.0",
     author="Antonín Šiška",
     author_email="siska.antonin.mail@example.com",
     description="3D FPS demo s PyQt6, kolizemi a minimapou",
-    long_description=open("README.md", encoding="utf-8").read(),
+    long_description=long_desc,
     long_description_content_type="text/markdown",
-    url="https://github.com/tvuj-github-username/game-engine",
+    url="https://github.com/antoninsiska/Game-Engine",
     packages=find_packages(),
     install_requires=["PyQt6>=6.5.0"],
     entry_points={
         "console_scripts": [
-            "game-engine=game-engine.main:main",
+            "game-engine=game_engine.main:main",
         ],
     },
     classifiers=[
